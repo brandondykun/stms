f97338d5f74f9b2d896e6a6778a0e2c916a8d6bc
Brandon Dykun <brandon.dykun@gmail.com>
2022-08-03 01:10:02 -0400

    Added Link wrapper around the user-container to redirect to user-info page.

--- stms/src/components/SectionContainer.jsx
+++ stms/src/components/SectionContainer.jsx
@@ -1,12 +1,16 @@
+import { Link } from "react-router-dom";
+
 const SectionContainer = ({ users, section }) => {
   return (
     <div className="section-container">
       <h2 className="section-title">{section}</h2>
       {users?.map((user) => {
         return (
-          <div className="user-container" key={user.id}>
-            {user.rank} {user.last_name}
-          </div>
+          <Link to={`/user-info/${user.id}`} className="text-link">
+            <div className="user-container" key={user.id}>
+              {user.rank} {user.last_name}
+            </div>
+          </Link>
         );
       })}
     </div>
