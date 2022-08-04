e2361fafd7712cd91e23b2d7499c3eb824857470
Brandon Dykun <brandon.dykun@gmail.com>
2022-08-04 14:46:55 -0400

    Created CommentsContainer component.

---
+++ stms/src/components/CommentsContainer.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useAuthContext } from "../context/AuthContext";
+import { useParams } from "react-router-dom";
+
+const CommentsContainer = ({ comments, category }) => {
+  const [filteredComments, setFilteredComments] = useState();
+
+  const { currentUser, setCurrentUser } = useAuthContext();
+
+  useEffect(() => {
+    setFilteredComments(
+      comments?.filter((comment) => {
+        return comment.category === category;
+      })
+    );
+  }, [comments]);
+
+  return (
+    <div className="comments-container">
+      <h2 className="section-title info-title">{category}</h2>
+      <ul className="comments-list">
+        {filteredComments?.length > 0 ? (
+          filteredComments.map((comment) => {
+            return (
+              <li className="comment-container" key={comment.id}>
+                {comment.text}
+                {currentUser.uid === comment.commentor_id && (
+                  <Link
+                    className="edit-link"
+                    to={`/comments/${comment.user_id}/edit/${comment.id}`}
+                  >
+                    -edit
+                  </Link>
+                )}
+              </li>
+            );
+          })
+        ) : (
+          <li className="comment-container no-comment">no comments yet</li>
+        )}
+      </ul>
+    </div>
+  );
+};
+
+export default CommentsContainer;
