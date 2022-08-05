af8e23c10a56d215fbf4016552e982f8b0a9467b
Brandon Dykun <brandon.dykun@gmail.com>
2022-08-05 11:35:31 -0400

    Moved form into CommentForm component.

--- stms/src/pages/EditCommentPage.jsx
+++ stms/src/pages/EditCommentPage.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { useEffect } from "react";
 import apiCalls from "../api/apiUtils";
 import { useNavigate } from "react-router-dom";
+import CommentForm from "../components/CommentForm";
 
 const formTemplate = {
   category: "",
@@ -43,47 +44,11 @@ const EditComment = () => {
   return (
     <div className="primary-content">
       <h1 className="page-title name-title">Edit Comment</h1>
-      <form onSubmit={handleSubmit} className="form add-comment-form">
-        <label htmlFor="category" className="form-label">
-          Category
-        </label>
-        <select
-          id="category"
-          aria-label="category"
-          type="text"
-          className="dark-input with-label"
-          value={formInputs.category}
-          onChange={(e) =>
-            setFormInputs({ ...formInputs, category: e.target.value })
-          }
-        >
-          {/* <option value="" disabled selected>
-            Category
-          </option> */}
-          <option value="CHARACTER">CHARACTER</option>
-          <option value="PRESENCE">PRESENCE</option>
-          <option value="INTELLECT">INTELLECT</option>
-          <option value="LEADERSHIP">LEADERSHIP</option>
-          <option value="DEVELOPS">DEVELOPS</option>
-          <option value="ACHIEVES">ACHIEVES</option>
-          <option value="OVERALL">OVERALL</option>
-        </select>
-
-        <textarea
-          className="text-area-input"
-          maxLength={250}
-          placeholder={"Comment..."}
-          rows={6}
-          type="text"
-          value={formInputs.text}
-          onChange={(e) =>
-            setFormInputs({ ...formInputs, text: e.target.value })
-          }
-        />
-        <button className="form-button login" type="submit">
-          Add Comment
-        </button>
-      </form>
+      <CommentForm
+        formInputs={formInputs}
+        setFormInputs={setFormInputs}
+        handleSubmit={handleSubmit}
+      />
     </div>
   );
 };
