8a5b247f33393552cf0afed8820782025f660432
Brandon Dykun <brandon.dykun@gmail.com>
2022-08-08 22:20:14 -0400

    Added UsersContextProvider wrapper.

--- stms/src/main.jsx
+++ stms/src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext";
+import { UsersContextProvider } from "./context/UsersContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   <AuthContextProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <UsersContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </UsersContextProvider>
   </AuthContextProvider>
   // </React.StrictMode>
 );
