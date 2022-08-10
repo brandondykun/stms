1d9ed3fa679c78fd5f5113f0a45da397f5ea6d8c
Brandon Dykun <brandon.dykun@gmail.com>
2022-08-09 20:56:27 -0400

    Added useAuthContext to get accountInfo. Used account info to conditionally render the Add Comment link. Added Font Awesome Icons to the Add Comment and Back links.

--- stms/src/pages/CommentsPage.jsx
+++ stms/src/pages/CommentsPage.jsx
@@ -1,15 +1,18 @@
-import { useEffect } from "react";
-import { useState } from "react";
-import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
 import apiCalls from "../api/apiUtils";
 import CommentsContainer from "../components/CommentsContainer";
-import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAnglesLeft, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { useAuthContext } from "../context/AuthContext";
 
 const CommentsPage = () => {
   const [comments, setComments] = useState([]);
   const [user, setUser] = useState([]);
 
   const { id } = useParams();
+
+  const { accountInfo } = useAuthContext();
 
   useEffect(() => {
     const getUserAndComments = async (id) => {
@@ -36,12 +39,23 @@ const CommentsPage = () => {
         <h1 className="page-title name-title">
           Comments for {user.rank} {user.last_name}
         </h1>
-        <Link to={`/comments/${id}/create`} className="comments-link">
-          Add Comment
-        </Link>
+        {accountInfo.is_staff && accountInfo.id !== id && (
+          <Link to={`/comments/${id}/create`} className="comments-link">
+            Add Comment
+            <span className="icon-margin-left">
+              <FontAwesomeIcon icon={faPlus} />
+            </span>
+          </Link>
+        )}
       </div>
-      <Link to={`/user-info/${id}`} className="comments-link">
-        Back
+
+      <Link to={`/user-info/${id}`} className="comments-link fit-link">
+        <div className="edit-button-container">
+          <span className="icon-margin-right">
+            <FontAwesomeIcon icon={faAnglesLeft} />
+          </span>
+          Back
+        </div>
       </Link>
       <div className="all-comments-container">
         <CommentsContainer comments={comments} category={"CHARACTER"} />
