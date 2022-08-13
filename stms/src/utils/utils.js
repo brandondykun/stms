b57bc4022e9223cf4dff22caf2393c70a5ade1e5
Brandon Dykun <brandon.dykun@gmail.com>
2022-08-12 21:51:19 -0400

    Refactored assignUnitPosition.

--- stms/src/utils/utils.js
+++ stms/src/utils/utils.js
@@ -25,141 +25,28 @@ utils.filter = (items, filterValue, value) => {
 };
 
 utils.assignUnitPosition = (user) => {
-  if (user.role == "BN FSO") {
-    return 1;
-  } else if (user.role == "BN FSNCO") {
-    return 2;
-  } else if (user.role == "BN FO") {
-    return 3;
-  } else if (user.role == "AFATDS") {
-    return 4;
-  } else if (user.role == "BN RTO") {
-    return 5;
-  } else if (user.section == "ALPHA" && user.role == "CO FSO") {
-    return 10;
-  } else if (user.section == "ALPHA" && user.role == "CO FSNCO") {
-    return 11;
-  } else if (user.section == "ALPHA" && user.role == "FO" && user.team == "1") {
-    return 12;
-  } else if (
-    user.section == "ALPHA" &&
-    user.role == "RTO" &&
-    user.team == "1"
-  ) {
-    return 13;
-  } else if (user.section == "ALPHA" && user.role == "FO" && user.team == "2") {
-    return 14;
-  } else if (
-    user.section == "ALPHA" &&
-    user.role == "RTO" &&
-    user.team == "2"
-  ) {
-    return 15;
-  } else if (user.section == "ALPHA" && user.role == "FO" && user.team == "3") {
-    return 16;
-  } else if (
-    user.section == "ALPHA" &&
-    user.role == "RTO" &&
-    user.team == "3"
-  ) {
-    return 17;
-  } else if (user.section == "BRAVO" && user.role == "CO FSO") {
-    return 20;
-  } else if (user.section == "BRAVO" && user.role == "CO FSNCO") {
-    return 21;
-  } else if (user.section == "BRAVO" && user.role == "FO" && user.team == "1") {
-    return 22;
-  } else if (
-    user.section == "BRAVO" &&
-    user.role == "RTO" &&
-    user.team == "1"
-  ) {
-    return 23;
-  } else if (user.section == "BRAVO" && user.role == "FO" && user.team == "2") {
-    return 24;
-  } else if (
-    user.section == "BRAVO" &&
-    user.role == "RTO" &&
-    user.team == "2"
-  ) {
-    return 25;
-  } else if (user.section == "BRAVO" && user.role == "FO" && user.team == "3") {
-    return 26;
-  } else if (
-    user.section == "BRAVO" &&
-    user.role == "RTO" &&
-    user.team == "3"
-  ) {
-    return 27;
-  } else if (user.section == "CHARLIE" && user.role == "CO FSO") {
-    return 30;
-  } else if (user.section == "CHARLIE" && user.role == "CO FSNCO") {
-    return 31;
-  } else if (
-    user.section == "CHARLIE" &&
-    user.role == "FO" &&
-    user.team == "1"
-  ) {
-    return 32;
-  } else if (
-    user.section == "CHARLIE" &&
-    user.role == "RTO" &&
-    user.team == "1"
-  ) {
-    return 33;
-  } else if (
-    user.section == "CHARLIE" &&
-    user.role == "FO" &&
-    user.team == "2"
-  ) {
-    return 34;
-  } else if (
-    user.section == "CHARLIE" &&
-    user.role == "RTO" &&
-    user.team == "2"
-  ) {
-    return 35;
-  } else if (
-    user.section == "CHARLIE" &&
-    user.role == "FO" &&
-    user.team == "3"
-  ) {
-    return 36;
-  } else if (
-    user.section == "CHARLIE" &&
-    user.role == "RTO" &&
-    user.team == "3"
-  ) {
-    return 37;
-  } else if (user.section == "DELTA" && user.role == "CO FSO") {
-    return 40;
-  } else if (user.section == "DELTA" && user.role == "CO FSNCO") {
-    return 41;
-  } else if (user.section == "DELTA" && user.role == "FO" && user.team == "1") {
-    return 42;
-  } else if (
-    user.section == "DELTA" &&
-    user.role == "RTO" &&
-    user.team == "1"
-  ) {
-    return 43;
-  } else if (user.section == "DELTA" && user.role == "FO" && user.team == "2") {
-    return 44;
-  } else if (
-    user.section == "DELTA" &&
-    user.role == "RTO" &&
-    user.team == "2"
-  ) {
-    return 45;
-  } else if (user.section == "DELTA" && user.role == "FO" && user.team == "3") {
-    return 46;
-  } else if (
-    user.section == "DELTA" &&
-    user.role == "RTO" &&
-    user.team == "3"
-  ) {
-    return 47;
-  } else return 50;
+  let position = 0;
+
+  if (user.role == "BN FSO") return 1;
+  if (user.role == "BN FSNCO") return 2;
+  if (user.role == "BN FO") return 3;
+  if (user.role == "AFATDS") return 4;
+  if (user.role == "BN RTO") return 5;
+  if (user.section === "UNASSIGNED" || user.section === "") return 50;
+
+  if (user.section === "ALPHA") position += 10;
+  if (user.section === "BRAVO") position += 20;
+  if (user.section === "CHARLIE") position += 30;
+  if (user.section === "DELTA") position += 40;
+
+  if (user.team === "1") position += 2;
+  if (user.team === "2") position += 4;
+  if (user.team === "3") position += 6;
+
+  if (user.role === "CO FSNCO") position += 1;
+  if (user.role === "RTO") position += 1;
+
+  return position;
 };
 
 export default utils;
