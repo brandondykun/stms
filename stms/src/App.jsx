45eb262f926ad78752b8ca14da7bf64fe0491ca6
Brandon Dykun <brandon.dykun@gmail.com>
2022-08-13 11:41:01 -0400

    Added UnAuthLayout, WebsiteLayout, AdminLAyout layouts and  AdminReassign and AdminOverview components.

--- stms/src/App.jsx
+++ stms/src/App.jsx
@@ -2,52 +2,58 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
-import NavBar from "./components/NavBar";
 import HomePage from "./pages/HomePage";
-import { useAuthContext } from "./context/AuthContext";
 import CreateAccountPage from "./pages/CreateAccountPage";
 import UserInfo from "./pages/UserInfo";
 import CommentsPage from "./pages/CommentsPage";
 import AddCommentPage from "./pages/AddCommentPage";
 import EditComment from "./pages/EditCommentPage";
 import EditUserPage from "./pages/EditUserPage";
-import AdminPage from "./pages/AdminPage";
 import AdminRoute from "./routes/AdminRoute";
 import RequireAuthRoute from "./routes/RequireAuthRoute";
 import AdminOrSelfRoute from "./routes/AdminOrSelfRoute";
 import AdminButNotSelfRoute from "./routes/AdminButNotSelfRoute";
+import UnAuthLayout from "./layouts/UnAuthLayout";
+import WebsiteLayout from "./layouts/WebsiteLayout";
+import AdminLayout from "./layouts/AdminLayout";
+import AdminReassign from "./components/AdminReassign";
+import AdminOverview from "./components/AdminOverview";
 
 function App() {
-  const { currentUser, accountInfo } = useAuthContext();
-
   return (
     <div className="App">
-      {accountInfo && <NavBar />}
       <Routes>
-        <Route path="login" element={<LoginPage />} />
-        <Route path="register" element={<SignUpPage />} />
+        <Route element={<UnAuthLayout />}>
+          <Route path="login" element={<LoginPage />} />
+          <Route path="register" element={<SignUpPage />} />
+        </Route>
         <Route element={<RequireAuthRoute />}>
-          <Route path="home" element={<HomePage />} />
-          <Route path="create-account/:id" element={<CreateAccountPage />} />
-          <Route path="user-info">
-            <Route path=":id" element={<UserInfo />} />
-            <Route element={<AdminOrSelfRoute />}>
-              <Route path=":id/edit" element={<EditUserPage />} />
+          <Route element={<WebsiteLayout />}>
+            <Route path="home" element={<HomePage />} />
+            <Route path="create-account/:id" element={<CreateAccountPage />} />
+            <Route path="user-info">
+              <Route path=":id" element={<UserInfo />} />
+              <Route element={<AdminOrSelfRoute />}>
+                <Route path=":id/edit" element={<EditUserPage />} />
+              </Route>
+            </Route>
+
+            <Route path="comments">
+              <Route element={<AdminOrSelfRoute />}>
+                <Route path=":id" element={<CommentsPage />} />
+              </Route>
+              <Route element={<AdminButNotSelfRoute />}>
+                <Route path=":id/edit/:cid" element={<EditComment />} />
+                <Route path=":id/create" element={<AddCommentPage />} />
+              </Route>
             </Route>
           </Route>
 
-          <Route path="comments">
-            <Route element={<AdminOrSelfRoute />}>
-              <Route path=":id" element={<CommentsPage />} />
+          <Route path="admin" element={<AdminRoute />}>
+            <Route element={<AdminLayout />}>
+              <Route path="overview" element={<AdminOverview />} />
+              <Route path="reassign" element={<AdminReassign />} />
             </Route>
-            <Route element={<AdminButNotSelfRoute />}>
-              <Route path=":id/edit/:cid" element={<EditComment />} />
-              <Route path=":id/create" element={<AddCommentPage />} />
-            </Route>
-          </Route>
-
-          <Route element={<AdminRoute />}>
-            <Route path="admin" element={<AdminPage />} />
           </Route>
         </Route>
       </Routes>
