47f100377711b08930e308ea3c10c886701199b5
Brandon Dykun <brandon.dykun@gmail.com>
2022-08-16 13:23:29 -0400

    Added useEffect logic to setUsers.

--- stms/src/routes/AdminRoute.jsx
+++ stms/src/routes/AdminRoute.jsx
@@ -1,5 +1,8 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuthContext } from "../context/AuthContext";
+import { useEffect } from "react";
+import { useUsersContext } from "../context/UsersContext";
+import apiCalls from "../api/apiUtils";
 
 /* 
 Route only allows access to children if the logged in user
@@ -8,10 +11,24 @@ is a staff user (accountInfo.is_staff).
 
 const AdminRoute = ({ redirectPath = "/home", children }) => {
   const { accountInfo } = useAuthContext();
+  const { setUsers } = useUsersContext();
 
   if (!accountInfo.is_staff) {
     return <Navigate to={redirectPath} replace />;
   }
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const res = await apiCalls.getAllUsers();
+      if (res.status === 200) {
+        setUsers(res.data);
+      } else {
+        console.error("Error", res.error);
+        // handle this error
+      }
+    };
+    getUsers();
+  }, []);
 
   return children ? children : <Outlet />;
 };
