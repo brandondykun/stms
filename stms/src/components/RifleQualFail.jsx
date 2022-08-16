b2470f6ff8550874814e4d1d53db9cb441794d46
Brandon Dykun <brandon.dykun@gmail.com>
2022-08-16 13:35:23 -0400

    Created RifleQualFail component.

---
+++ stms/src/components/RifleQualFail.jsx
@@ -0,0 +1,27 @@
+import { useUsersContext } from "../context/UsersContext";
+
+const RifleQualFail = () => {
+  const { users, setUsers, active, setActive } = useUsersContext();
+
+  const userList = users.filter((user) => {
+    return user.m4_qual < 23;
+  });
+  return (
+    <div className="dlc-incomplete-list">
+      <div className="chart-title gold-title">M4 Qual Fail</div>
+      {userList?.length > 0 ? (
+        userList.map((user) => {
+          return (
+            <div className="admin-chart-list-item" key={user.id}>
+              {user.rank} {user.last_name}
+            </div>
+          );
+        })
+      ) : (
+        <div>none</div>
+      )}
+    </div>
+  );
+};
+
+export default RifleQualFail;
