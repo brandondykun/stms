11a2e172d884de3fcf681fb430a88414a83d699a
Brandon Dykun <brandon.dykun@gmail.com>
2022-08-16 13:04:53 -0400

    Renamed component function from AdminPage to AdminReassign.

--- stms/src/components/AdminReassign.jsx
+++ stms/src/components/AdminReassign.jsx
@@ -1,31 +1,10 @@
-import apiCalls from "../api/apiUtils";
-import { useEffect } from "react";
-import { useUsersContext } from "../context/UsersContext";
 import AdminSection from "./AdminSection";
 import AdminHQSection from "./AdminHQSection";
 import UnassignedSection from "./UnassignedSection";
 
-const AdminPage = () => {
-  const { users, setUsers, active, setActive } = useUsersContext();
-
-  useEffect(() => {
-    const getUsers = async () => {
-      const res = await apiCalls.getAllUsers();
-      if (res.status === 200) {
-        setUsers(res.data);
-      } else {
-        console.error("Error", res.error);
-        // handle this error
-      }
-    };
-    getUsers();
-  }, []);
-
+const AdminReassign = () => {
   return (
     <div className="primary-content">
-      {/* <div className="title-link-container">
-        <h1 className="page-title name-title">Admin Page</h1>
-      </div> */}
       <div className="admin-sections-container">
         <AdminHQSection />
         <AdminSection sectionName={"ALPHA"} />
@@ -41,4 +20,4 @@ const AdminPage = () => {
   );
 };
 
-export default AdminPage;
+export default AdminReassign;
