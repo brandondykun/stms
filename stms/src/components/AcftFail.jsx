cbecf02f07452c79762e26da119aff2e418b4516
Brandon Dykun <brandon.dykun@gmail.com>
2022-08-17 00:34:05 -0400

    Created AcftFail component.

---
+++ stms/src/components/AcftFail.jsx
@@ -0,0 +1,29 @@
+import { useUsersContext } from "../context/UsersContext";
+
+const AcftFail = () => {
+  const { users, setUsers, active, setActive } = useUsersContext();
+
+  const userList = users.filter((user) => {
+    return !user.acft_pass;
+  });
+  return (
+    <div className="dlc-incomplete-list">
+      <div className="chart-title gold-title">ACFT Fail</div>
+      <div className="admin-list-items-container">
+        {userList?.length > 0 ? (
+          userList.map((user) => {
+            return (
+              <div className="admin-chart-list-item" key={user.id}>
+                {user.rank} {user.last_name}
+              </div>
+            );
+          })
+        ) : (
+          <div>none</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AcftFail;
