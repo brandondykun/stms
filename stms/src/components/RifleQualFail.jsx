123968ad3b33403a244c6f9cb5ea89ebf98de49e
Brandon Dykun <brandon.dykun@gmail.com>
2022-08-16 23:25:46 -0400

    Added admin-list-items-container div wrapper around the userList.

--- stms/src/components/RifleQualFail.jsx
+++ stms/src/components/RifleQualFail.jsx
@@ -9,17 +9,19 @@ const RifleQualFail = () => {
   return (
     <div className="dlc-incomplete-list">
       <div className="chart-title gold-title">M4 Qual Fail</div>
-      {userList?.length > 0 ? (
-        userList.map((user) => {
-          return (
-            <div className="admin-chart-list-item" key={user.id}>
-              {user.rank} {user.last_name}
-            </div>
-          );
-        })
-      ) : (
-        <div>none</div>
-      )}
+      <div className="admin-list-items-container">
+        {userList?.length > 0 ? (
+          userList.map((user) => {
+            return (
+              <div className="admin-chart-list-item" key={user.id}>
+                {user.rank} {user.last_name}
+              </div>
+            );
+          })
+        ) : (
+          <div>none</div>
+        )}
+      </div>
     </div>
   );
 };
