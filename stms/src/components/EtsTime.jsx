760c5a80fa2ff3b697f66923ad8529178298717e
Brandon Dykun <brandon.dykun@gmail.com>
2022-08-17 23:26:50 -0400

    Created EtsTime component.

---
+++ stms/src/components/EtsTime.jsx
@@ -0,0 +1,50 @@
+import { useUsersContext } from "../context/UsersContext";
+import dayjs from "dayjs";
+import utils from "../utils/utils";
+
+const EtsTime = () => {
+  const { users } = useUsersContext();
+
+  const sortedUsers = users.sort((a, b) => {
+    return a.ets.seconds - b.ets.seconds;
+  });
+
+  return (
+    <div className="ets-table-container">
+      {console.log(users)}
+      <table className="ets-table">
+        <tbody className="ets-table-body">
+          <tr className="ets-table-record">
+            <th>Name</th>
+            <th className="hide-800">Rank</th>
+            <th className="hide-500">ETS Date</th>
+            <th>ETS</th>
+          </tr>
+          {sortedUsers.map((user) => {
+            const etsDate = dayjs(user.ets.seconds * 1000);
+            const now = dayjs();
+            const diff = etsDate.diff(now, "day");
+            const formattedDiff = utils.getFormattedStringFromDays(diff);
+            return (
+              <tr
+                className={`ets-table-record ${
+                  diff <= 90 ? "red-bcg" : diff <= 365 ? "yellow-bcg" : ""
+                }`}
+                key={user.id}
+              >
+                <td className="ets-table-td-name">
+                  {user.last_name}, {user.first_name}
+                </td>
+                <td className="hide-800">{user.rank}</td>
+                <td className="hide-500">{etsDate.format("DD MMM YYYY")}</td>
+                <td>{formattedDiff}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default EtsTime;
