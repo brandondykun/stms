838ca8add39129f6af405684c3e73f911d8d06d3
Brandon Dykun <brandon.dykun@gmail.com>
2022-08-17 23:26:25 -0400

    Created DeleteAccount component.

---
+++ stms/src/components/DeleteAccount.jsx
@@ -0,0 +1,12 @@
+import { useUsersContext } from "../context/UsersContext";
+
+const DeleteAccount = () => {
+  const { users } = useUsersContext();
+  return (
+    <div className="primary-content">
+      <h1>Coming Soon</h1>
+    </div>
+  );
+};
+
+export default DeleteAccount;
