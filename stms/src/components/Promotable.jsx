75f27f8a230513264b0b3c8e0bbdb4bae70eb14d
Brandon Dykun <brandon.dykun@gmail.com>
2022-08-17 23:28:05 -0400

    Created Promotable component.

---
+++ stms/src/components/Promotable.jsx
@@ -0,0 +1,31 @@
+import { useUsersContext } from "../context/UsersContext";
+import utils from "../utils/utils";
+
+const Promotable = () => {
+  const { users } = useUsersContext();
+
+  const userList = users.filter((user) => {
+    return utils.isPromotable(user) === true;
+  });
+
+  return (
+    <div className="dlc-incomplete-list">
+      <div className="chart-title gold-title">Promotable</div>
+      <div className="admin-list-items-container">
+        {userList?.length > 0 ? (
+          userList.map((user) => {
+            return (
+              <div className="admin-chart-list-item" key={user.id}>
+                {user.rank} {user.last_name}
+              </div>
+            );
+          })
+        ) : (
+          <div>none</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Promotable;
