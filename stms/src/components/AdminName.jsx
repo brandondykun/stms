a072e6010ffeff11a33892f70ddde1e855d4b77c
Brandon Dykun <brandon.dykun@gmail.com>
2022-08-19 16:47:12 -0400

    Added useEffect to set active user to an empty string when the component unmounts. Added select wrapper around the select inputs. This was used to add and position a font awesome down arrow for the drop down inputs.

--- stms/src/components/AdminName.jsx
+++ stms/src/components/AdminName.jsx
@@ -1,13 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import apiCalls from "../api/apiUtils";
 import { useUsersContext } from "../context/UsersContext";
 import utils from "../utils/utils";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
 
 const AdminName = ({ user }) => {
   const [expand, setExpand] = useState(false);
   const [userInfo, setUserInfo] = useState({ ...user });
 
   const { users, setUsers, active, setActive } = useUsersContext();
+
+  useEffect(() => {
+    return () => {
+      setActive("");
+    };
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -114,73 +122,88 @@ const AdminName = ({ user }) => {
         <div className={`expand-contract ${expand ? " expanded" : ""}`}>
           <form className="assign-form" onSubmit={handleSubmit}>
             <div className="assign-form-inputs-container">
-              <select
-                id="section"
-                aria-label="section"
-                placeholder="section"
-                className="dark-input with-label assign-input"
-                value={userInfo.section}
-                onChange={(e) =>
-                  // setUserInfo((info) => ({ ...info, section: e.target.value }))
-                  handleSectionChange(e)
-                }
-              >
-                <option value="" disabled>
-                  SECTION
-                </option>
-                <option value="BN STAFF">BN STAFF</option>
-                <option value="ALPHA">ALPHA</option>
-                <option value="BRAVO">BRAVO</option>
-                <option value="CHARLIE">CHARLIE</option>
-                <option value="UNASSIGNED">UNASSIGNED</option>
-              </select>
-
-              <select
-                id="team"
-                aria-label="team"
-                placeholder="team"
-                className="dark-input with-label assign-input"
-                value={userInfo.team}
-                onChange={(e) => handleTeamChange(e)}
-              >
-                <option value="" disabled>
-                  TEAM
-                </option>
-                {showTeamHqOption && <option value="HQ">HQ</option>}
-                {showTeamOptions && <option value="1">1</option>}
-                {showTeamOptions && <option value="2">2</option>}
-                {showTeamOptions && <option value="3">3</option>}
-                {showUnassigned && (
+              <div className="select-wrapper">
+                <select
+                  id="section"
+                  aria-label="section"
+                  placeholder="section"
+                  className="dark-input with-label assign-input"
+                  value={userInfo.section}
+                  onChange={(e) =>
+                    // setUserInfo((info) => ({ ...info, section: e.target.value }))
+                    handleSectionChange(e)
+                  }
+                >
+                  <option value="" disabled>
+                    SECTION
+                  </option>
+                  <option value="BN STAFF">BN STAFF</option>
+                  <option value="ALPHA">ALPHA</option>
+                  <option value="BRAVO">BRAVO</option>
+                  <option value="CHARLIE">CHARLIE</option>
                   <option value="UNASSIGNED">UNASSIGNED</option>
-                )}
-              </select>
+                </select>
+                <div className="select-custom-icon-reassign">
+                  <FontAwesomeIcon icon={faAngleDown} size="sm" />
+                </div>
+              </div>
 
-              <select
-                id="role"
-                aria-label="role"
-                placeholder="role"
-                className="dark-input with-label assign-input"
-                value={userInfo.role}
-                onChange={(e) =>
-                  setUserInfo((info) => ({ ...info, role: e.target.value }))
-                }
-              >
-                <option value="" disabled>
-                  ROLE
-                </option>
-                {showBnRole && <option value="BN FSO">BN FSO</option>}
-                {showBnRole && <option value="BN FSNCO">BN FSNCO</option>}
-                {showBnRole && <option value="AFATDS">AFATDS</option>}
-                {showBnRole && <option value="BN FO">BN FO</option>}
-                {showBnRole && <option value="BN RTO">BN RTO</option>}
-                {showCoHqRole && <option value="CO FSO">CO FSO</option>}
-                {showCoHqRole && <option value="CO FSNCO">CO FSNCO</option>}
-                {showCoTeamRole && <option value="FO">FO</option>}
-                {showCoTeamRole && <option value="RTO">RTO</option>}
-                {showUnassigned && (
-                  <option value="UNASSIGNED">UNASSIGNED</option>
-                )}
-              </select>
+              <div className="select-wrapper">
+                <select
+                  id="team"
+                  aria-label="team"
+                  placeholder="team"
+                  className="dark-input with-label assign-input"
+                  value={userInfo.team}
+                  onChange={(e) => handleTeamChange(e)}
+                >
+                  <option value="" disabled>
+                    TEAM
+                  </option>
+                  {showTeamHqOption && <option value="HQ">HQ</option>}
+                  {showTeamOptions && <option value="1">1</option>}
+                  {showTeamOptions && <option value="2">2</option>}
+                  {showTeamOptions && <option value="3">3</option>}
+                  {showUnassigned && (
+                    <option value="UNASSIGNED">UNASSIGNED</option>
+                  )}
+                </select>
+                <div className="select-custom-icon-reassign">
+                  <FontAwesomeIcon icon={faAngleDown} size="sm" />
+                </div>
+              </div>
+
+              <div className="select-wrapper">
+                <select
+                  id="role"
+                  aria-label="role"
+                  placeholder="role"
+                  className="dark-input with-label assign-input"
+                  value={userInfo.role}
+                  onChange={(e) =>
+                    setUserInfo((info) => ({ ...info, role: e.target.value }))
+                  }
+                >
+                  <option value="" disabled>
+                    ROLE
+                  </option>
+                  {showBnRole && <option value="BN FSO">BN FSO</option>}
+                  {showBnRole && <option value="BN FSNCO">BN FSNCO</option>}
+                  {showBnRole && <option value="AFATDS">AFATDS</option>}
+                  {showBnRole && <option value="BN FO">BN FO</option>}
+                  {showBnRole && <option value="BN RTO">BN RTO</option>}
+                  {showCoHqRole && <option value="CO FSO">CO FSO</option>}
+                  {showCoHqRole && <option value="CO FSNCO">CO FSNCO</option>}
+                  {showCoTeamRole && <option value="FO">FO</option>}
+                  {showCoTeamRole && <option value="RTO">RTO</option>}
+                  {showUnassigned && (
+                    <option value="UNASSIGNED">UNASSIGNED</option>
+                  )}
+                </select>
+                <div className="select-custom-icon-reassign">
+                  <FontAwesomeIcon icon={faAngleDown} size="sm" />
+                </div>
+              </div>
             </div>
             <div className="assign-form-button-container">
               <button className="assign-form-button">Submit</button>
