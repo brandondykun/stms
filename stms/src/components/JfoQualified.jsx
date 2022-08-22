91b060891f43bc9afcdb8520d9866ebb627a9476
Brandon Dykun <brandon.dykun@gmail.com>
2022-08-22 12:41:07 -0400

    Added class name to None div.

--- stms/src/components/JfoQualified.jsx
+++ stms/src/components/JfoQualified.jsx
@@ -19,7 +19,7 @@ const JfoQualified = () => {
             );
           })
         ) : (
-          <div>none</div>
+          <div className="color-dark-placeholder">None</div>
         )}
       </div>
     </div>
