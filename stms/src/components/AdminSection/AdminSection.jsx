084a191dc35ffc4d33de84a8e4da79de3ae5871c
Brandon Dykun <brandon.dykun@gmail.com>
2022-09-17 02:02:09 -0400

    Moved AdminSection into its own folder and added css module.

--- stms/src/components/AdminSection/AdminSection.jsx
+++ stms/src/components/AdminSection/AdminSection.jsx
@@ -1,15 +1,16 @@
-import { useUsersContext } from "../context/UsersContext";
-import utils from "../utils/utils";
-import AdminTeam from "./AdminTeam";
+import { useUsersContext } from "../../context/UsersContext";
+import utils from "../../utils/utils";
+import AdminTeam from "../AdminTeam";
+import styles from "./AdminSection.module.css";
 
 const AdminSection = ({ sectionName }) => {
-  const { users, setUsers, active, setActive } = useUsersContext();
+  const { users } = useUsersContext();
 
   const filteredUsers = utils.filter(users, "section", sectionName);
 
   return (
-    <div className="admin-section">
-      <h2 className="admin-section-title">{sectionName}</h2>
+    <div className={styles.adminSection}>
+      <h2 className={styles.adminSectionTitle}>{sectionName}</h2>
       <AdminTeam users={filteredUsers} team={"HQ"} />
       <AdminTeam users={filteredUsers} team={"1"} />
       <AdminTeam users={filteredUsers} team={"2"} />
