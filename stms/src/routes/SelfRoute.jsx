fd083562b26afd208c17d20e1cf64a6a3e830a32
Brandon Dykun <brandon.dykun@gmail.com>
2022-09-28 21:01:54 -0400

    Edited description comment.

--- stms/src/routes/SelfRoute.jsx
+++ stms/src/routes/SelfRoute.jsx
@@ -2,9 +2,8 @@ import { Navigate, Outlet, useParams } from "react-router-dom";
 import { useAuthContext } from "../context/AuthContext";
 
 /* 
-Route only allows access to children if the logged in user is a
-staff user (accountInfo.is_staff) or if the logged in user is trying 
-to access their own information.
+Route only allows access to children if the logged in 
+user is trying to access their own information.
 */
 
 const SelfRoute = ({ redirectPath = "/home", children }) => {
