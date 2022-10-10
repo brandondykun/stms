74c088f6ef3b287dab5759741488f50f1a3bc702
Brandon Dykun <brandon.dykun@gmail.com>
2022-10-10 09:24:00 -0400

    Added AnimatePresence wrapper.

--- stms/src/components/AdminTeam/AdminTeam.jsx
+++ stms/src/components/AdminTeam/AdminTeam.jsx
@@ -1,6 +1,7 @@
 import utils from "../../utils/utils";
 import AdminName from "../AdminName";
 import styles from "./AdminTeam.module.css";
+import { AnimatePresence, motion } from "framer-motion";
 
 const AdminTeam = ({ users, team }) => {
   const filteredUsers = utils.filter(users, "team", team).sort((a, b) => {
@@ -10,13 +11,25 @@ const AdminTeam = ({ users, team }) => {
   return (
     <div className={styles.adminTeam}>
       <h3 className={styles.adminTeamTitle}>Team: {team}</h3>
-      {filteredUsers.length > 0 ? (
-        filteredUsers.map((user) => {
-          return <AdminName user={user} key={user.id} />;
-        })
-      ) : (
-        <div className={styles.noneAssignedText}>No one assigned</div>
-      )}
+      <AnimatePresence mode="popLayout">
+        {filteredUsers.length > 0 ? (
+          filteredUsers.map((user) => {
+            return (
+              <motion.div
+                layout
+                animate={{ scale: 1, opacity: 1 }}
+                exit={{ scale: 0.8, opacity: 0 }}
+                transition={{ type: "just" }}
+                key={user.id}
+              >
+                <AdminName user={user} key={user.id} />
+              </motion.div>
+            );
+          })
+        ) : (
+          <div className={styles.noneAssignedText}>No one assigned</div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
