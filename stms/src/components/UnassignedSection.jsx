cab95e0ec5ef1394596d2ee12e26cb1e639d9a63
Brandon Dykun <brandon.dykun@gmail.com>
2022-10-10 09:25:29 -0400

    Added AnimatePresence wrapper.

--- stms/src/components/UnassignedSection.jsx
+++ stms/src/components/UnassignedSection.jsx
@@ -1,6 +1,7 @@
 import { useUsersContext } from "../context/UsersContext";
 import AdminName from "./AdminName";
 import utils from "../utils/utils";
+import { AnimatePresence, motion } from "framer-motion";
 
 const UnassignedSection = () => {
   const { users, setUsers, active, setActive } = useUsersContext();
@@ -12,13 +13,25 @@ const UnassignedSection = () => {
   return (
     <div className="admin-section">
       <h2 className="admin-section-title">{sectionName}</h2>
-      {filteredUsers.length > 0 ? (
-        filteredUsers.map((user) => {
-          return <AdminName user={user} key={user.id} />;
-        })
-      ) : (
-        <div className="none-assigned-text">No one assigned</div>
-      )}
+      <AnimatePresence mode="popLayout">
+        {filteredUsers.length > 0 ? (
+          filteredUsers.map((user) => {
+            return (
+              <motion.div
+                layout
+                animate={{ scale: 1, opacity: 1 }}
+                exit={{ scale: 0.8, opacity: 0 }}
+                transition={{ type: "just" }}
+                key={user.id}
+              >
+                <AdminName user={user} key={user.id} />
+              </motion.div>
+            );
+          })
+        ) : (
+          <div className="none-assigned-text">No one assigned</div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
