afa87c49f22d3bb636a11419b0937199d81d6eea
Brandon Dykun <brandon.dykun@gmail.com>
2022-10-15 12:31:36 -0400

    Added data-cy cypress selectors.

--- stms/src/pages/ExamResultsPage/ExamResultsPage.jsx
+++ stms/src/pages/ExamResultsPage/ExamResultsPage.jsx
@@ -30,40 +30,51 @@ const ExamResultsPage = () => {
 
   return (
     <div className="primary-content">
-      <h1 className={styles.pageTitle}>Exam Results</h1>
-      <div className={styles.resultsSummaryText}>
+      <h1 data-cy="results-title" className={styles.pageTitle}>
+        Exam Results
+      </h1>
+      <div data-cy="results-summary" className={styles.resultsSummaryText}>
         You got {numberCorrect} out of {numberOfQuestions} questions correct.
       </div>
-      <div className={styles.percentContainer}>
+      <div data-cy="results-percent" className={styles.percentContainer}>
         {(percent * 100).toFixed(0)}%
       </div>
 
       <div className={styles.textAndButtonContainer}>
         {incorrectQuestions?.length > 0 && (
           <div className={styles.buttonContainer}>
-            <button onClick={handleExit} className={styles.button}>
+            <button
+              data-cy="top-exit-button"
+              onClick={handleExit}
+              className={styles.button}
+            >
               Exit Exam
             </button>
           </div>
         )}
 
         {incorrectQuestions?.length > 0 ? (
-          <div className={styles.questionsTitle}>
+          <div data-cy="wrong-answers-text" className={styles.questionsTitle}>
             Here are the questions you need to work on:
           </div>
         ) : (
-          <div className={styles.resultsSummaryText}>
+          <div data-cy="all-correct-text" className={styles.resultsSummaryText}>
             Good work. You made that look easy.
           </div>
         )}
       </div>
-      {incorrectQuestions?.map((question) => {
+      {incorrectQuestions?.map((question, qid) => {
         return (
           <div className={styles.container} key={question.id}>
             <div className={styles.questionContainer}>
-              <div className={styles.questionText}>{question.question}</div>
+              <div
+                data-cy={`question-text-${qid}`}
+                className={styles.questionText}
+              >
+                {question.question}
+              </div>
               <div className={styles.optionsContainer}>
-                {question.options.map((option) => {
+                {question.options.map((option, oid) => {
                   const conditionalClass =
                     option === question.guess
                       ? styles.wrongGuess
@@ -71,7 +82,11 @@ const ExamResultsPage = () => {
                       ? styles.correctAnswer
                       : styles.otherOption;
                   return (
-                    <div className={conditionalClass} key={option}>
+                    <div
+                      data-cy={`question-${qid}-option-${oid}`}
+                      className={conditionalClass}
+                      key={option}
+                    >
                       {option}
                     </div>
                   );
@@ -82,7 +97,11 @@ const ExamResultsPage = () => {
         );
       })}
       <div className={styles.buttonContainer}>
-        <button onClick={handleExit} className={styles.button}>
+        <button
+          data-cy="bottom-exit-button"
+          onClick={handleExit}
+          className={styles.button}
+        >
           Exit Exam
         </button>
       </div>
