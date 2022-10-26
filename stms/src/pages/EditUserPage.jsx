58744d9239d8dbd00088f10edc736d2a58e88546
Brandon Dykun <brandon.dykun@gmail.com>
2022-10-25 23:59:10 -0400

    Fixed bug where editing user info changed acft_pass to false.

--- stms/src/pages/EditUserPage.jsx
+++ stms/src/pages/EditUserPage.jsx
@@ -80,7 +80,10 @@ const EditUserPage = () => {
     const dorTimeStamp = utils.getTimeStamp(formInputs.dor);
     const etsTimeStamp = utils.getTimeStamp(formInputs.ets);
     const isStaff = utils.isStaff(formInputs.role);
-    const acftPass = formInputs.acft_pass === "true" ? true : false;
+    const acftPass =
+      formInputs.acft_pass === "true" || formInputs.acft_pass === true
+        ? true
+        : false;
 
     const data = {
       ...formInputs,
